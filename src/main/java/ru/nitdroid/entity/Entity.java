466087012251ed19e9f2aa91579ad5b385f1413b
NITDroid1995 <roman111095@gmail.com>
2019-11-08 20:23:35 +0300

    Делаем моба

--- src/main/java/ru/nitdroid/entity/Entity.java
+++ src/main/java/ru/nitdroid/entity/Entity.java
@@ -1,7 +1,10 @@
 package ru.nitdroid.entity;
 
 import cpw.mods.fml.common.registry.EntityRegistry;
+import net.minecraft.entity.EntityList;
 import ru.nitdroid.common.Main;
+
+import static ru.nitdroid.common.Main.instance;
 
 public class Entity {
 
@@ -12,3 +15,12 @@ public class Entity {
     }
 
 }
+    public static void registerEntity(Class entityClass, String name, int primaryColor, int secondaryColor)
+    {
+        int entityID = EntityRegistry.findGlobalUniqueEntityId();
+        long seed = name.hashCode();
+
+        EntityRegistry.registerGlobalEntityID(entityClass, name, entityID);
+        EntityRegistry.registerModEntity(entityClass, name, entityID, instance, 64, 1, true); //эта строка не нужна(зачем она?)
+        EntityList.entityEggs.put(Integer.valueOf(entityID), new EntityList.EntityEggInfo(entityID, primaryColor, secondaryColor));
+    }
